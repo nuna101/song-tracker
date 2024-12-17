08ef1430b2e2b0af49a772ee5a01ca43d14390e5
nuna101 <nunajidapanak@gmail.com>
2024-12-17 20:55:14 +0700

    "sc"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/song-tracker/",
+  // base: "/song-tracker/",
   plugins: [react()],
 })
